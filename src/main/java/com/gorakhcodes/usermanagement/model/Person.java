9157969ad553e9a62f09b5aeca185ba31ee311bc
Gorakh <gorakhgupta999@gmail.com>
2024-05-15 00:03:32 +0530

    Added model class and util class

--- src/main/java/com/gorakhcodes/usermanagement/model/Person.java
+++ src/main/java/com/gorakhcodes/usermanagement/model/Person.java
@@ -1,4 +1,63 @@
 package com.gorakhcodes.usermanagement.model;
 
+import java.util.Date;
+
 public class Person {
+
+    private int id;
+    private String name;
+    private int age;
+//    private Date dateOfBirth;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+//    public Date getDateOfBirth() {
+//        return dateOfBirth;
+//    }
+//
+//    public void setDateOfBirth(Date dateOfBirth) {
+//        this.dateOfBirth = dateOfBirth;
+//    }
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+//        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Person() {
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+//                ", dateOfBirth=" + dateOfBirth +
+                '}';
+    }
 }
--- src/main/java/com/gorakhcodes/usermanagement/utils/PersonUtils.java
+++ src/main/java/com/gorakhcodes/usermanagement/utils/PersonUtils.java
@@ -1,4 +1,15 @@
 package com.gorakhcodes.usermanagement.utils;
 
+import com.gorakhcodes.usermanagement.model.Person;
+
 public class PersonUtils {
+    Person [] persons = new Person[5];
+    int personCount = 0;
+
+    public PersonUtils(){
+        persons[0] = new Person(1, "Abc", 24);
+        personCount++;
+        persons[1] = new Person(2, "xyz", 13);
+        personCount++;
+    }
 }
